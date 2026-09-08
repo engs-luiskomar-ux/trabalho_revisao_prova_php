c18bee7ba5b0409a5ca3ba1aa3f5a19e7b327737
Luis <engs-luiskomar@camporeal.edu.br>
2026-09-07 22:28:47 -0300

    atc 1 - criar as rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function() {
+    return 'pagina sobre';
+});
+
+Route::get('/alunos', function(){
+    return 'lista alunos';
+});
+
+Route::get('/contato', function(){
+    return 'pagina contato';
+});
+
