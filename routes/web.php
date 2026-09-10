31030ad5b57f81a01f26f7ad725302c2daf1cd05
Luis <engs-luiskomar@camporeal.edu.br>
2026-09-09 21:27:43 -0300

    ATV 8 - Criar paginas principais

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlunoController;
 
 Route::get('/', function() {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/sobre', function() {
@@ -28,4 +28,3 @@ Route::get('/usuario/{$id}', function($id){
 });
 
 Route::resource('alunos', AlunoController::class);
-
